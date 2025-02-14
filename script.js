9cade43e6428f11ff549979f9b765cf1ea23c150
Maddi <maddi.fitzmaurice@gmail.com>
2025-02-14 21:59:07 +1100

    Index Removal

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-if (window.location.href.includes(".html")) {
+// if (window.location.href.includes(".html")) {
 
-    var index;
+//     var index;
 
-    if (window.location.href.includes("index")) {
-        index = window.location.href.indexOf("index");
-    }
-    else {
-        index = window.location.href.indexOf(".html");
-    }
+//     if (window.location.href.includes("index")) {
+//         index = window.location.href.indexOf("index");
+//     }
+//     else {
+//         index = window.location.href.indexOf(".html");
+//     }
   
-    // Wait 5ms so any same page anchors can load
-    setTimeout(()=>{
-        window.location.href = window.location.href.slice(0, index);
-    }, 5);
-}
+//     // Wait 5ms so any same page anchors can load
+//     setTimeout(()=>{
+//         window.location.href = window.location.href.slice(0, index);
+//     }, 5);
+// }
 
 function anchor(elementName) {
     document.getElementById(elementName).scrollIntoView();
