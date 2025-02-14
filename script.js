0aa2d7a214636ac3d6d5bbb4a03b2b13e4ce2486
Maddi <maddi.fitzmaurice@gmail.com>
2025-02-14 20:16:38 +1100

    About Change
    Moved About Me section to a separate page

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 
-
-
-
-// if (window.location.href.includes(".html")) {
-
-//     var index;
-
-//     if (window.location.href.includes("index"))
-//     {
-//         index = window.location.href.indexOf("index.html");
-//     }
-//     else {
-//         index = window.location.href.indexOf(".html");
-//     }
-
-//     // if (window.location.href.includes("#"))
-//     // {
-//     //     var anchorName = window.location.hash;
-//     //     anchorName = anchorName.replace("#", "");
-//     // }
-
-//     window.location.href = window.location.href.slice(0, index);
-// }
+if (window.location.href.includes(".html")) {
+    // window.location.href = window.location.href.replace("index.html", "");
+    window.location.href = window.location.href.replace(".html", "");
+}
 
 
 
