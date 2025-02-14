1a36471840a93e1d31b4d2d865662bd565efbb76
Maddi <maddi.fitzmaurice@gmail.com>
2025-02-14 20:29:54 +1100

    JS .html Removal Test 19

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
 if (window.location.href.includes(".html")) {
-    const index = window.location.href.indexOf(".html");
+
+    var index;
+
+    if (window.location.href.includes("index")) {
+        index = window.location.href.indexOf("index");
+    }
+    else {
+        index = window.location.href.indexOf(".html");
+    }
   
     // Wait 5ms so any same page anchors can load
     setTimeout(()=>{
         window.location.href = window.location.href.slice(0, index);
     }, 5);
 }
-
-
 
 function anchor(elementName) {
     document.getElementById(elementName).scrollIntoView();
